95d1120049595356a39ea46f625195d45f9f0ada
Andrew Meakovski <meako689@gmail.com>
2014-11-08 11:56:48 +0200

    task api

--- fckupio/api/urls.py
+++ fckupio/api/urls.py
@@ -1,1 +1,10 @@
- 
+from django.conf.urls.defaults import *
+from piston.resource import Resource
+from .handlers import TaskHandler
+
+task_handler = Resource(TaskHandler)
+
+urlpatterns = patterns('',
+   url(r'^task/(?P<task_id>\d+)/', task_handler),
+   url(r'^tasks/', task_handler),
+)
--- fckupio/fckupio/urls.py
+++ fckupio/fckupio/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('mysite.api.urls')),
+    url(r'^api/', include('api.urls')),
 )
