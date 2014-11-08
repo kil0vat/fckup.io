d3159fbb33fe7619ffa15b68684ed57dd894c62b
Andrew Meakovski <meako689@gmail.com>
2014-11-08 22:53:35 +0200

    logout view

--- fckupio/core/views.py
+++ fckupio/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout as auth_logout
 
 
 def index(request):
@@ -9,3 +10,8 @@ def app(request):
 
 def auth_success(request):
     return render(request, 'auth_success.html', {})
+
+def logout(request):
+    """Logs out user"""
+    auth_logout(request)
+    return render_to_response('home.html', {}, RequestContext(request))
--- fckupio/fckupio/urls.py
+++ fckupio/fckupio/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from core.views import index, app, auth_success
+from core.views import index, app, auth_success, logout
 
 from tasks.api import TaskResource
 
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', index, name='index'),
     url(r'^app$', app, name='app'),
+    url(r'^logout$', logout, name='logout'),
     url(r'^auth_success$', auth_success, name='auth_success'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
