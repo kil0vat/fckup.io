af2ab306a6204a400c2102cae4058a7929dc79f6
Andrew Meakovski <meako689@gmail.com>
2014-11-08 11:20:31 +0200

    template rendering

--- fckupio/fckupio/urls.py
+++ fckupio/fckupio/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from core.views import index
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'fckupio.views.home', name='home'),
+    url(r'^$', index, name='index'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
