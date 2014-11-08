eb09d8db221958386f8272f6c1efe8d402220d00
Andrew Meakovski <meako689@gmail.com>
2014-11-08 17:59:39 +0200

    facebook login

--- fckupio/core/views.py
+++ fckupio/core/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'index.html', {})
 
-
 def app(request):
     return render(request, 'app.html', {})
+
+def auth_success(request):
+    return render(request, 'auth_success.html', {})
