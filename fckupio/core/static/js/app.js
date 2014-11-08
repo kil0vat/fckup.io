ac6257e0d84d960a1fb4236f9fd2394bd0cb6b6b
DimaWaverley <dmytro.lubenets@gmail.com>
2014-11-08 19:33:59 +0200

    bug

--- fckupio/core/static/js/app.js
+++ fckupio/core/static/js/app.js
@@ -1,4 +1,4 @@
-window.app = angular.module('app', ['restangular', 'ui.router', 'appControllers', 'appServices']);
+window.app = angular.module('app', ['restangular', 'ui.router', 'appControllers', 'appServices', 'oitozero.ngSweetAlert']);
 
 // Application Level State
 app.config(['$stateProvider', '$urlRouterProvider', 'RestangularProvider', function($stateProvider, $urlRouterProvider, RestangularProvider) {
@@ -8,41 +8,52 @@ app.config(['$stateProvider', '$urlRouterProvider', 'RestangularProvider', funct
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-    .state('app', {
-      url: '/',
-      controller: 'AppCtrl',
-      templateUrl: staticUrl + 'js/partials/app.html',
-    })
-    .state('deal', {
-      url: '/deal/:id',
-      templateUrl: staticUrl + 'js/partials/deal.html',
-      controller: function($stateParams, dealsDS, $scope, $rootScope){
+  .state('app', {
+    url: '/',
+    controller: 'AppCtrl',
+    templateUrl: staticUrl + 'js/partials/app.html',
+  })
+  .state('deal', {
+    url: '/deal/:id',
+    templateUrl: staticUrl + 'js/partials/deal.html',
+    controller: function($stateParams, dealsDS, $scope, $rootScope, SweetAlert, $state){
 
-        $rootScope.staticUrl = staticUrl;
+      $rootScope.staticUrl = staticUrl;
 
-        $rootScope.rightPaneActive = false;
+      $rootScope.rightPaneActive = false;
 
-        dealsDS.getDeal($stateParams.id).then(function(data){
-          $scope.deal = data;
-          $scope.hideButtons = $scope.deal.participant.pk !== window.userId;
-        }).catch(function(){});
+      dealsDS.getDeal($stateParams.id).then(function(data){
+        $scope.deal = data;
+        $scope.hideButtons = $scope.deal.participant.pk !== window.userId;
+      }).catch(function(){});
 
-        $scope.complete = function(){
-          if($scope.deal.id){
-            $scope.deal.complete = true;
-            $scope.deal.fucked_up = false;
-            $scope.deal.put();
-          }
-        };
+      $scope.complete = function(){
+        if($scope.deal.id){
+          $scope.deal.complete = true;
+          $scope.deal.fucked_up = false;
+          $scope.deal.put().then(function(){
+            showMessage();
+          });
+        }
+      };
 
-        $scope.fuckedUp = function(){
-          if($scope.deal.id){
-            $scope.deal.complete = true;
-            $scope.deal.fucked_up = true;
-            $scope.deal.put();
-          }
-        };
+      $scope.fuckedUp = function(){
+        if($scope.deal.id){
+          $scope.deal.complete = true;
+          $scope.deal.fucked_up = true;
+          $scope.deal.put();
+        }
+      };
+
+      function showMessage(){
+        SweetAlert.swal({
+          title: "Greetings, dude!",
+          text: "You've just approved deal! Nice work",
+          imageUrl: staticUrl + 'img/smiling.png'
+        },function(){
+          $state.go("app");
+        });
       }
-    });
+    } }
 
-}]);
+  }]);
